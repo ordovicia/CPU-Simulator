af35450f6c76cedf9f8f65af2065e4ae43804ac7
Hidehito Yabuuchi <hdht.ybuc@gmail.com>
2016-10-23 18:27:29 +0900

    Add util_test

---
+++ test/util_test.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+#include "util.hpp"
+
+using namespace std;
+
+void myassert_impl(bool b, int l);
+
+#define myassert(b) \
+    myassert_impl((b), __LINE__)
+
+int main()
+{
+    cout << hex;
+
+    myassert(bitset(0x00ff00ff, 0, 8) == 0x00000000);
+    myassert(bitset(0x00ff00ff, 0, 16) == 0x000000ff);
+    myassert(bitset(0x00ff00ff, 0, 24) == 0x0000ff00);
+    myassert(bitset(0x00ff00ff, 0, 32) == 0x00ff00ff);
+    myassert(bitset(0x00ff00ff, 8, 16) == 0x000000ff);
+    myassert(bitset(0x00ff00ff, 8, 24) == 0x0000ff00);
+    myassert(bitset(0x00ff00ff, 8, 32) == 0x00ff00ff);
+    myassert(bitset(0x00ff00ff, 16, 24) == 0x00000000);
+    myassert(bitset(0x00ff00ff, 16, 32) == 0x000000ff);
+    myassert(bitset(0x00ff00ff, 24, 32) == 0x000000ff);
+
+    myassert(bitset64(0x00ff00ffff00ff00, 0, 32) == 0x00ff00ff);
+    myassert(bitset64(0x00ff00ffff00ff00, 32, 64) == 0xff00ff00);
+
+    myassert(signExt5(0x0f) == 0x0f);
+    myassert(signExt5(0x1f) == 0xffffffff);
+
+    return 0;
+}
+
+void myassert_impl(bool b, int l)
+{
+    if (b)
+        return;
+    cerr << "Assertion failed @ " << l << endl;
+}
