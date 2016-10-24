0038c1ae7fabbc0e8d01b22d99a282192febd17c
Hidehito Yabuuchi <hdht.ybuc@gmail.com>
2016-10-24 10:55:57 +0900

    Fix bug in c_eq_s.cpp

--- src/instruction/c_eq_s.cpp
+++ src/instruction/c_eq_s.cpp
@@ -7,8 +7,8 @@ Simulator::State Simulator::c_eq_s(Instruction inst)
 
     auto op = decodeR(inst);
 
-    auto b1 = btof(m_state_iter->freg.at(op.rd));
-    auto b2 = btof(m_state_iter->freg.at(op.rs));
+    auto b1 = ftob(m_state_iter->freg.at(op.rd));
+    auto b2 = ftob(m_state_iter->freg.at(op.rs));
 
     new_state.pc += 4;
     new_state.fpcond = b1 == b2;
