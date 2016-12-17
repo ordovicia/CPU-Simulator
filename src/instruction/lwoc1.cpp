fe3d55500fea6184df3489e71f3ed456d39236b5
Hidehito Yabuuchi <hdht.ybuc@gmail.com>
2016-12-17 15:41:14 +0900

    新しいISAに対応; LWOC1, SWOC1命令追加

---
+++ src/instruction/lwoc1.cpp
@@ -0,0 +1,21 @@
+#include "simulator.hpp"
+#include "util.hpp"
+
+Simulator::State Simulator::lwoc1(Instruction inst)
+{
+    auto new_state = *m_state_iter;
+    new_state.memory_patch = MemoryPatch{};
+
+    auto op = decodeR(inst);
+    auto addr = (m_state_iter->reg.at(op.rs) + m_state_iter->reg.at(op.rt)) / 4;
+
+    new_state.pc += 4;
+    try {
+        new_state.freg.at(op.rd)
+            = btof(static_cast<uint32_t>(m_memory.at(addr)));
+    } catch (std::out_of_range e) {
+        FAIL("# Memory index out of range\n" << e.what());
+    }
+
+    return new_state;
+}
---
+++ src/instruction/swoc1.cpp
@@ -0,0 +1,23 @@
+#include "simulator.hpp"
+#include "util.hpp"
+
+Simulator::State Simulator::swoc1(Instruction inst)
+{
+    auto new_state = *m_state_iter;
+    new_state.memory_patch = MemoryPatch{};
+
+    auto op = decodeR(inst);
+    auto addr = (m_state_iter->reg.at(op.rt) + m_state_iter->reg.at(op.rd)) / 4;
+
+    new_state.pc += 4;
+    try {
+        auto pre_mem = m_memory.at(addr);
+        m_memory.at(addr)
+            = static_cast<int32_t>(ftob(m_state_iter->freg.at(op.rs)));
+        new_state.memory_patch = {true, addr, pre_mem};
+    } catch (std::out_of_range e) {
+        FAIL("# Memory index out of range\n" << e.what());
+    }
+
+    return new_state;
+}
