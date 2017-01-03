5fc263def6468f116471e1db353fef096da80ca2
Hidehito Yabuuchi <hdht.ybuc@gmail.com>
2017-01-04 05:38:52 +0900

    Add multi and divi

---
+++ src/instruction/multi.cpp
@@ -0,0 +1,16 @@
+#include "simulator.hpp"
+#include "util.hpp"
+
+Simulator::State Simulator::multi(Instruction inst)
+{
+    auto new_state = *m_state_iter;
+    new_state.memory_patch = MemoryPatch{};
+
+    auto op = decodeI(inst);
+
+    new_state.pc += 4;
+    new_state.reg.at(op.rt) = m_state_iter->reg.at(op.rs)
+                              * static_cast<int32_t>(signExt(op.immediate, 16));
+
+    return new_state;
+}
