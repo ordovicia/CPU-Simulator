a0409c36beca36f5b93d37b96c13089c2de92fc4
Hidehito Yabuuchi <hdht.ybuc@gmail.com>
2017-01-14 14:27:51 +0900

    Fail with error log when input file not given

--- src/instruction/in.cpp
+++ src/instruction/in.cpp
@@ -8,9 +8,11 @@ Simulator::State Simulator::in(Instruction inst)
 
     auto op = decodeR(inst);
 
+    if (!m_infile.is_open())
+        FAIL("# Input file not opened\n");
     auto in_ = m_infile.get();
     if (m_infile.eof())
-        FAIL("Input error\n");
+        FAIL("# Input file reached EOF\n");
 
     new_state.pc += 4;
     new_state.reg.at(op.rd) = static_cast<int32_t>(in_);
