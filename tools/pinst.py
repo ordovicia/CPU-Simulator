7a620639cd6b985275cd619e9603cadf95ebd193
Hidehito Yabuuchi <hdht.ybuc@gmail.com>
2017-01-20 12:45:54 +0900

    Modify pinst

--- tools/pinst.py
+++ tools/pinst.py
@@ -5,9 +5,16 @@ from gen_instruction import insts
 
 fn = sys.argv[1] if len(sys.argv) > 1 else 'instruction.log'
 
+counts = {}
 with open(fn) as f:
-    ls = [ls.split() for ls in f.readlines()]
-    ls = {int(inst): int(n) for (inst, n) in ls}
-    for i in range(64):
-        if i in ls:
-            print(insts[i][0], ls[i])
+    for l in f:
+        if l[0] == '#':
+            continue
+        inst, cnt = [int(x) for x in l.split()]
+        counts[inst] = cnt
+
+for inst in range(64):
+    if inst in insts:
+        name = insts[inst][0]
+        cnt = counts.get(inst, 0)
+        print(name, cnt)
--- tools/pinst.sh
+++ tools/pinst.sh
@@ -5,5 +5,11 @@ set -e
 root=$(git rev-parse --show-toplevel)
 tools=$root/tools
 
-python3 $tools/pinst.py > pinst.log
+if [ $# -ge 1 ]; then
+    f=$1
+else
+    f="instruction.log"
+fi
+
+python3 $tools/pinst.py $f > pinst.log
 gnuplot -persist $tools/pinst.gpl
