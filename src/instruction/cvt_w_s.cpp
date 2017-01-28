03039b4db0c81a1d8904ee5b17959a49e22f8a37
Hidehito Yabuuchi <hdht.ybuc@gmail.com>
2017-01-28 22:07:40 +0900

    CVT.W.Sが負の無限大方向に丸めるようにした

--- src/instruction/cvt_w_s.cpp
+++ src/instruction/cvt_w_s.cpp
@@ -1,3 +1,4 @@
+#include <cmath>
 #include "simulator.hpp"
 #include "util.hpp"
 
@@ -7,7 +8,7 @@ Simulator::PreState Simulator::cvt_w_s(Instruction inst)
 
     auto pre_state = makePreFRegState(op.rt);
 
-    m_freg.at(op.rt) = btof(static_cast<int32_t>(m_freg.at(op.rs)));
+    m_freg.at(op.rt) = btof(static_cast<int32_t>(std::floor(m_freg.at(op.rs))));
     m_pc += 4;
 
     return pre_state;
