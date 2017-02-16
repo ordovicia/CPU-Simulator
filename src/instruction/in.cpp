0e694302ca64086ac7c765cb4fe6a54b43c34a0b
Hidehito Yabuuchi <hdht.ybuc@gmail.com>
2017-02-16 14:51:24 +0900

    IN命令で、下位8bitだけレジスタを更新するようにした

--- src/instruction/in.cpp
+++ src/instruction/in.cpp
@@ -7,13 +7,18 @@ Simulator::PreState Simulator::in(Instruction inst)
 
     auto pre_state = makePrePCState(m_pc);
 
+    // #ifndef FELIS_SIM_NO_ASSERT
     if (!m_infile.is_open())
         FAIL("# Error: Input file not opened\n");
-    auto in_ = m_infile.get();
     if (m_infile.eof())
         FAIL("# Error: Input file reached EOF\n");
+    // #endif
 
-    m_reg.at(op.rd) = static_cast<int32_t>(in_);
+    char in_;
+    m_infile.get(in_);
+    // m_reg.at(op.rd) = static_cast<int32_t>(in_);
+    m_reg.at(op.rd) = (m_reg.at(op.rd) & (~0u << 8)) | in_;
+
     m_pc += 4;
 
     return pre_state;
