85fe4f91b92a511e8c1ac98477a8f0a84db9ac02
Hidehito Yabuuchi <hdht.ybuc@gmail.com>
2017-02-16 22:04:26 +0900

    Use std::nearbyint() in CVT.W.S

--- src/instruction/cvt_w_s.cpp
+++ src/instruction/cvt_w_s.cpp
@@ -8,7 +8,8 @@ Simulator::PreState Simulator::cvt_w_s(Instruction inst)
 
     auto pre_state = makePreFRegState(op.rt);
 
-    m_freg.at(op.rt) = btof(static_cast<int32_t>(m_freg.at(op.rs)));
+    m_freg.at(op.rt)
+        = btof(static_cast<int32_t>(std::nearbyint(m_freg.at(op.rs))));
     m_pc += 4;
 
     return pre_state;
