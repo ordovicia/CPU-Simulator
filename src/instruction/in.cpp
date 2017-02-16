ab4c1dc3a0130eb57d18c7e34bcc0db02727161e
Hidehito Yabuuchi <hdht.ybuc@gmail.com>
2017-02-16 16:57:06 +0900

    Fix IN

--- src/instruction/in.cpp
+++ src/instruction/in.cpp
@@ -7,17 +7,18 @@ Simulator::PreState Simulator::in(Instruction inst)
 
     auto pre_state = makePrePCState(m_pc);
 
-    // #ifndef FELIS_SIM_NO_ASSERT
+#ifndef FELIS_SIM_NO_ASSERT
     if (!m_infile.is_open())
         FAIL("# Error: Input file not opened\n");
     if (m_infile.eof())
         FAIL("# Error: Input file reached EOF\n");
-    // #endif
+#endif
 
     char in_;
     m_infile.get(in_);
     // m_reg.at(op.rd) = static_cast<int32_t>(in_);
-    m_reg.at(op.rd) = (m_reg.at(op.rd) & (~0u << 8)) | in_;
+    m_reg.at(op.rd) = (m_reg.at(op.rd) & (~0u << 8))
+                      | (static_cast<unsigned char>(in_) & 0xffu);
 
     m_pc += 4;
 
